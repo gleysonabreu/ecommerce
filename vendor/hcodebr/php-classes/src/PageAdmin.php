7568bab991fe135bf99b244b2b5e0dbccc53c85b
gleysonabreu <gleyson_datu@hotmail.com>
2018-10-18 10:22:33 -0300

    criamos pageadmin e tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,15 @@
+<?php
+	namespace Hcode;
+
+	/**
+	 * PageAdmin
+	 */
+	class PageAdmin extends Page
+	{
+		
+		function __construct($opts = array(), $tpl_dir = '/views/admin/')
+		{
+			parent::__construct($opts, $tpl_dir);
+		}
+	}
+?>
