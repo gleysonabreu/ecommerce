ee5686043ad8a68c66ff1f14af97735f575da1ff
gleysonabreu <gleyson_datu@hotmail.com>
2018-10-18 09:17:38 -0300

    Create a new Page class

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php 
 
 require_once("vendor/autoload.php");
+	
+	use \Slim\Slim;
+	use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
+	$page = new Page();
 
-	$s = $sql->select("SELECT * FROM tb_users");;
-
-		echo json_encode($s);
+	$page->setTpl("index");
 
 });
 
