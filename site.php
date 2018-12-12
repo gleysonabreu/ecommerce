18a8d294e55813639f3f1e9d0082df63192ba91f
gleysonabreu <gleyson_datu@hotmail.com>
2018-12-12 11:16:48 -0300

    Integração Paypal html

--- site.php
+++ site.php
@@ -230,7 +230,16 @@ $app->post("/checkout", function (){
     ]);
    $order->save();
 
-    header("Location: /order/".$order->getidorder()."/pagseguro");
+   switch ((int)$_POST['payment-method']){
+       case 1:
+           header("Location: /order/".$order->getidorder()."/pagseguro");
+           break;
+       case 2:
+           header("Location: /order/".$order->getidorder()."/paypal");
+           break;
+   }
+
+
     exit;
 });
 
@@ -255,6 +264,27 @@ $app->get("/order/:idorder/pagseguro", function($idorder){
             'areaCode'=>substr($order->getnrphone(), 0, 2),
             'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphne()))
         ]
+    ]);
+
+});
+
+$app->get("/order/:idorder/paypal", function($idorder){
+
+    User::verifyLogin(false);
+
+    $order  = new Order();
+    $order->getOrder((int)$idorder);
+
+    $cart = $order->getCart();
+
+    $page = new Page([
+        "header"=>false,
+        "footer"=>false
+    ]);
+    $page->setTpl("payment-paypal", [
+        "order"=>$order->getValues(),
+        "products"=>$cart->getProducts(),
+        "cart"=>$cart->getValues()
     ]);
 
 });
